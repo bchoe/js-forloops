002a4901ae6155e8e6a67349b8897222ec624709
Choe <Choe@Choes-MacBook-Air.local>
2018-07-11 19:46:16 -1000

    finished #5

--- exercises.js
+++ exercises.js
@@ -64,7 +64,14 @@ leaders(presidents);
 Declare a variable named `stringOfNumbers` and assign its value to an empty string. 
 
 Write a for-loop that concatenates a Number value into that string on each iteration, starting at `10` and continuing up to and including `20`. Console.log your result. It should read "1011121314151617181920"*/
+var stringOfNumbers = '';
 
+for (var i = 10; i < 21; i++){
+  stringOfNumbers += i;
+  console.log(i);
+}
+
+console.log(stringOfNumbers);
 
 /* 5) Even Stevens
 Declare a variable named `evenNumArr` and assign its value to an empty array. 
@@ -73,6 +80,15 @@ Write a for-loop that will push even numbers to the `evenNumArr` array. We want 
 Console.log your results.
 */
 
+var evenNumArr = [];
+for (var i = 1; i < 101; i++){
+  if(i%2===0){
+    evenNumArr.push(i)
+  }
+}
+
+console.log(evenNumArr);
+console.log(evenNumArr.length);
 
 /* 6) Up the Odds
 Declare a variable named `oddSum` and assign it to the Number value 0.
